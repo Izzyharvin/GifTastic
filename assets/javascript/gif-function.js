65cfb1f058ccd94c863a02f676e5ca9ba8b7d158
Israel Harvin <izzy84.ih@gmail.com>
2019-07-12 21:49:11 -0400

    Added css to style the page

--- assets/javascript/gif-function.js
+++ assets/javascript/gif-function.js
@@ -17,21 +17,35 @@ $("button").on("click", function () {
     //Tells the computer not to run it yet but to hold on to it when the computer get the response back
     .then(function (response) {
         console.log(response)
+        //The gifs-appear-here shows shows the gifs of the actor they choose  or created and the .empty clears the 
+        //gifs when clicked on the other actor button.
         $("#gifs-appear-here").empty()
+        //This is a for loop and its going to show 10 gifs instead of one.
         for (i = 0; i < 10; i++) {
+            //Made a gif variable that is equal to the method which is going to grab the fixed height of the source
+            //image.
             var gif = $("<img class='gif-Image' src='" + response.data[i].images.fixed_height.url + "'>")
+            //This is the method gif that makes the attribute data animated for fixed height.
             $(gif).attr("data-animated", response.data[i].images.fixed_height.url)
+            //This is the method gif that makes the attribute data animated for fixed height still. 
             $(gif).attr("data-still", response.data[i].images.fixed_height_still.url)
+            //This is the method gif that makes the attribute data state.
             $(gif).attr("data-state")
+            //This is the method gif that makes the attribute data rating and filters the rating.
             $(gif).attr("data-rating", response.data[i].rating)
+            //This is the method gif-appear-here that append the gif.
             $("#gifs-appear-here").append(gif)
         }
     });
 });
 
+//This is the .on click method and the .gif-Image and the function
 $(document).on("click", '.gif-Image', function() {
     console.log("this works");
+    //Made a variable called state which is equal to the method (this) and the .attribute is data-animated. 
     var state = $(this).attr("");
+    //This is a if/else statement that if the state equal still then it will animate but for else if the data is
+    //clicked it will turn from animate to still.
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animated"));
         $(this).attr("data-state", "animate");
